b27f49113930389ff6d29f398b32f4a60e3e8549
Mengsen-W <mengsen_wang@163.com>
2020-04-26 22:09:36 +0800

    deamon

---
+++ proc/ngx_daemon.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2020-04-26 20:38:45
+ * @Last Modified by: Mengsen.Wang
+ * @Last Modified time: 2020-04-26 22:09:18
+ * @Description: Deamon process
+ */
+
+int ngx_daemon(){
+  
+}
